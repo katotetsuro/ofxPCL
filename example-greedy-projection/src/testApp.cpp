9af09de9c4e6fd6765e8c03c00800e4111a2546b
KatoTetsuro <katotetsuro@team-lab.com>
2015-02-10 01:32:49 +0900

    fix example-greedy-projection

--- example-greedy-projection/src/testApp.cpp
+++ example-greedy-projection/src/testApp.cpp
@@ -8,8 +8,8 @@ void testApp::setup()
 {
 	dispRaw = false;
 	
-	ofxPCL::PointCloud cloud(new ofxPCL::PointCloud::value_type);
-	ofxPCL::PointNormalPointCloud cloud_with_normals(new ofxPCL::PointNormalPointCloud::value_type);
+	ofxPCL::PointCloud cloud(new ofxPCL::PointCloud::element_type);
+	ofxPCL::PointNormalPointCloud cloud_with_normals(new ofxPCL::PointNormalPointCloud::element_type);
 	
 	cloud = ofxPCL::loadPointCloud<ofxPCL::PointCloud>("bun0.pcd");
 	
